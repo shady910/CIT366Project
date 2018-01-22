05b4c16483b438e0f75e17a97ff9cbe42a91acff
shady910 <dod14005@byui.edu>
2018-01-21 20:49:43 -0700

    January 21- Almost got the project, just have to figure why the info from the contacts.model is not pulling. Am going to test out the panels and see why the button is not aligning on the top right corectly.

---
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {Contact} from '../contacts.model';
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+  export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact(
+      '1',
+      'Bro Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      'https://web.byui.edu/Directory/Employee/jacksonk.jpg',
+      null),
+    new Contact(
+      '2',
+      'Bro Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      'https:/web.byui.edu/Directory/Employee/barzeer.jpb',
+      null)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(contacts: Contact){
+    //this.selectedContactEvent.emit(contact);
+  }
+
+}
