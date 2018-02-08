23e405ba35ab852c8b0fbb1bd798579bf42d6095
shady910 <dod14005@byui.edu>
2018-02-08 11:32:57 -0700

    February 8- Directive dropdown working as specified. Assignment 4 complete, no errors.

--- cms/src/app/shared/dropdown.directive.ts
+++ cms/src/app/shared/dropdown.directive.ts
@@ -4,9 +4,15 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
-
+  private isOpen = false;
+  @HostBinding('class.open') get opened(){
+    return this.isOpen;
+}
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
-}
+    @HostListener('mouseleave') close(){
+      this.isOpen = !this.isOpen;
+    }
+  }
+
