7426775b44c0626e305ead899b4102a6ccf30458
shady910 <dod14005@byui.edu>
2018-02-14 15:42:00 -0700

    Feb 14- Fixed the header. Got most of the contacts working. About to move on to the group part.

--- cms/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Contact} from "../contacts.model";
+import { ContactService} from "../contact.service";
+import {ActivatedRoute, Router, Params} from "@angular/router";
+import {NgForm} from "@angular/forms";
+import {Document} from "../../documents/document.model";
+
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-edit.component.css']
 })
 export class ContactEditComponent implements OnInit {
+contact: Contact = null;
+groupContacts: Contact[] = [];
+  original: Contact;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(private contactsService: ContactService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params: Params)  => {this.original = this.contactsService.getContact(params['id']);
+      if (this.original) {
+        this.editMode = true;
+        this.contact = JSON.parse(JSON.stringify(this.original));
+      }
+    });
   }
+// The onSubmit method
+  onSubmit(form: NgForm){
+    const contact: Contact = new Contact(String(this.contactsService.getMaxId()),
+      form.value.name, form.value.email, form.value.phone, form.value.url,  null);
 
+
+    if (this.editMode) {
+      this.contactsService.updateContact(this.original, contact);
+    }
+    else {
+      this.contactsService.addContact(contact);
+    }
+    this.router.navigate(['/contacts']);
+  }
+  // the onCancel method
+  onCancel() {
+    this.router.navigate(['/contacts']);
+  }
 }
--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, Output, EventEmitter} from "@angular/core";
 import {Contact} from './contacts.model';
 import { MOCKCONTACTS} from "./MOCKCONTACTS";
+import {Subject} from "rxjs/Subject";
 
 
 @Injectable()
@@ -8,8 +9,43 @@ export class ContactService {
   contacts: Contact[] = [];
   @Output() contactSelectedEvent: EventEmitter<Contact> = new EventEmitter<Contact>();
   @Output() contactChange: EventEmitter<Contact[]> = new EventEmitter<Contact[]>();
+  // the subject event
+  contactListChangedEvent: Subject<Contact[]> = new Subject<Contact[]>();
+  maxContactId: number;
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
+  }
+  // get maxId
+  getMaxId(): number {
+    let maxId: number = 0;
+
+    this.contacts.forEach((contact: Contact) => {
+      let currentId: number = Number(contact.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    });
+    return maxId;
+  }
+
+// add Contact
+  addContact(contact: Contact){
+    if (contact) {
+      contact.id = String(++this.maxContactId);
+
+      this.contacts.push(contact);
+      this.contactListChangedEvent.next(this.getContacts());
+    }
+  }
+  //update Contact
+  updateContact(original: Contact, updated: Contact){
+    let pos;
+    if (original && updated && ( pos = this.contacts.indexOf(original)) >= 0){
+      updated.id = original.id;
+      this.contacts[pos] = updated;
+      this.contactListChangedEvent.next(this.getContacts());
+    }
   }
 
   getContacts(): Contact[] {
